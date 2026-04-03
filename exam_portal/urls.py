7fa7b80b5cb729aadfafb766427739989b2a123d
Your Name <you@example.com>
2026-04-03 22:21:56 +0530

    Fix: Re-adding home page redirect properly

--- exam_portal/urls.py
+++ exam_portal/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from accounts import views as account_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', account_views.dashboard_redirect, name='index'),
     path('', include('accounts.urls')),
     path('exams/', include('exams.urls', namespace='exams')),
     path('results/', include('results.urls', namespace='results')),
